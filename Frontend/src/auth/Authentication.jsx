3ac4f6b57e50367cc194bfca75ba9905338d3804
Ritesh jha <rjamasterstudios@gmail.com>
2025-07-24 15:55:48 +0530

    authentication with clerk

---
+++ Frontend/src/auth/Authentication.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { SignIn, SignUp, SignedIn, SignedOut } from '@clerk/clerk-react'
+
+export default function Authentication() {
+  return (
+    <div className='auth-container'>
+      <SignedOut>
+           <SignIn routing='path' path='/sign-in'/>
+           <SignUp routing='path' path='/sign-up'/>
+      </SignedOut>
+      <SignedIn>
+           <div className='redirect-message'>
+                <p>You are already signed in.</p>
+           </div>
+      </SignedIn>
+    </div>
+  )
+}
---
+++ Frontend/src/challenge/MCQChallenge.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MCQChallenge() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
