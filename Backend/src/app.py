0a8b3fbfa68fd7a477a26a74e62afea269092e1e
Ritesh jha <rjamasterstudios@gmail.com>
2025-07-30 00:17:11 +0530

    CORS added

--- Backend/src/app.py
+++ Backend/src/app.py
@@ -1,12 +1,24 @@
-from fastapi import FastAPI, Response, Request
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .routes import challenge, webhooks
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
 app = FastAPI()
+ 
+FRONTEND_URL = os.getenv("FRONTEND_URL")
+origins=[]
+if FRONTEND_URL:
+    origins.append(FRONTEND_URL)
+origins.append("http://localhost:5173")
+
+
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
